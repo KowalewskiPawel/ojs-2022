1cc76e7c86bb6e2b6878a6e182351f8839bfb19b
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-03-18 21:36:24 +0100

    solution 3 added WIP

--- 3/index.js
+++ 3/index.js
@@ -1,12 +1,15 @@
-function magicTemplate() {
-
+function magicTemplate(strings, ...rest) {
+  let sentence = "";
+  return sentence;
 }
 
-const firstName = 'James'
-const lastName = 'Bond'
+const firstName = "James";
+const lastName = "Bond";
 
-const update = magicTemplate`My name is ${{ lastName }}. ${{ firstName }} ${{ lastName }}.`
+const update = magicTemplate`My name is ${{ lastName }}. ${{ firstName }} ${{
+  lastName,
+}}.`;
 
-update({ firstName: 'George' })
-update({ lastName: 'Weasley' })
-update({ firstName: 'Ron' })
+update({ firstName: "George" });
+update({ lastName: "Weasley" });
+update({ firstName: "Ron" });
